e9e7949ffebd91792396bdcd187e7132acf46369
AlanProgrammer93 <76537764+AlanProgrammer93@users.noreply.github.com>
2021-08-05 09:55:21 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDVAwtxcIIZkkXGZJYLYjNCdmswXaQmzXQ",
-    authDomain: "lista-gastos-df159.firebaseapp.com",
-    projectId: "lista-gastos-df159",
-    storageBucket: "lista-gastos-df159.appspot.com",
-    messagingSenderId: "1013217225461",
-    appId: "1:1013217225461:web:260d3727916079712ef3bc"
+    // Datos de firebase
   };
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
